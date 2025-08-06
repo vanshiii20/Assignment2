67a0bd5117f4a5636c209c538ec83b8e06419f12
vanshiii20 <160826723+vanshiii20@users.noreply.github.com>
2025-08-06 20:47:30 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+number=(input("Enter a number :"))
+number = int(number)
+
+if  number% 2 == 0 :
+    print (number,"is an even number")
+else :
+    print(number,"is an odd number")
---
+++ task2.py
@@ -0,0 +1,5 @@
+
+total_num = 0
+for current_num in range (1, 50 +1):
+    total_num += current_num
+print("The sum of number from 1 to 50 is :" , total_num)
